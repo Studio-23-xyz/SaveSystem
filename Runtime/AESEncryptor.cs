c6c779a79d0e8f086d5ed0fa21346067cf9e7378
zubEpty <eptyt4@gmail.com>
2023-10-06 17:06:44 +0600

    Initial commit

---
+++ Runtime/AESEncryptor.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Studio23.SS2.SaveSystem.Core
+{
+    public class AESEncryptor
+    {
+        private readonly string key;
+        private readonly string iv;
+
+        public AESEncryptor(string key, string iv)
+        {
+            if (key.Length != 16 || iv.Length != 16)
+                throw new ArgumentException("Key and IV must be 16 bytes (128 bits) each.");
+
+            this.key = key;
+            this.iv = iv;
+        }
+
+        public string Encrypt(string plainText)
+        {
+            using (var aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            using (var aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+                var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (var msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/LocalSaveManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using Studio23.SS2.SaveSystem.Core;
+using UnityEngine;
+
+namespace Studio23.SS2.SaveSystem.Core
+{
+    public class LocalSaveManager
+    {
+        private readonly string _saveFilePath;
+        private readonly bool _useEncryption;
+        private readonly AESEncryptor _encryptor;
+
+        public LocalSaveManager(string saveFileName, bool enableEncryption = false, string encryptionKey = null,
+            string encryptionIV = null)
+        {
+            // Define the path where save files will be stored (e.g., in the Application.persistentDataPath).
+            _saveFilePath = Path.Combine(Application.persistentDataPath, saveFileName);
+            _useEncryption = enableEncryption;
+
+            if (_useEncryption && !string.IsNullOrEmpty(encryptionKey) && !string.IsNullOrEmpty(encryptionIV))
+                _encryptor = new AESEncryptor(encryptionKey, encryptionIV);
+        }
+
+        // Save data to a local file using Newtonsoft.Json serialization.
+        public void Save<T>(T data)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+                if (_useEncryption && _encryptor != null)
+                    // Encrypt the JSON data before saving.
+                    json = _encryptor.Encrypt(json);
+
+                File.WriteAllText(_saveFilePath, json);
+                Debug.Log("Data saved locally.");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error saving data: " + ex.Message);
+            }
+        }
+
+        // Load data from a local file using Newtonsoft.Json deserialization.
+        public T Load<T>()
+        {
+            try
+            {
+                if (File.Exists(_saveFilePath))
+                {
+                    var json = File.ReadAllText(_saveFilePath);
+
+                    if (_useEncryption && _encryptor != null)
+                        // Decrypt the JSON data before deserialization.
+                        json = _encryptor.Decrypt(json);
+
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+
+                Debug.LogWarning("No save data found.");
+                return default;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error loading data: " + ex.Message);
+                return default;
+            }
+        }
+    }
+}
