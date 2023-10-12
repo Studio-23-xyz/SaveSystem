75e2d0a7560b7da2194b93ce7bb6481ffd53366c
zubEpty <eptyt4@gmail.com>
2023-10-12 18:04:22 +0600

    updated savesystem class

--- Assets/Packages/com.studio23.ss2.savesystem/Runtime/Utility/AESEncryptor.cs
+++ Assets/Packages/com.studio23.ss2.savesystem/Runtime/Utility/AESEncryptor.cs
@@ -2,68 +2,57 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using Cysharp.Threading.Tasks;
 
 
 namespace Studio23.SS2.SaveSystem.Utilities
 {
     public class AESEncryptor
     {
-        private readonly string key;
-        private readonly string iv;
+        private readonly string _key;
+        private readonly string _IV;
 
         public AESEncryptor(string key, string iv)
         {
             if (key.Length != 16 || iv.Length != 16)
                 throw new ArgumentException("Key and IV must be 16 bytes (128 bits) each.");
 
-            this.key = key;
-            this.iv = iv;
+            _key = key;
+            _IV = iv;
         }
 
-        public string Encrypt(string plainText)
+        public async UniTask<string> Encrypt(string plainText)
         {
-            using (var aesAlg = Aes.Create())
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(_key);
+            aesAlg.IV = Encoding.UTF8.GetBytes(_IV);
+
+            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using var msEncrypt = new MemoryStream();
+            await using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
             {
-                aesAlg.Key = Encoding.UTF8.GetBytes(key);
-                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
-
-                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                using (var msEncrypt = new MemoryStream())
+                await using (var swEncrypt = new StreamWriter(csEncrypt))
                 {
-                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (var swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            swEncrypt.Write(plainText);
-                        }
-                    }
-
-                    return Convert.ToBase64String(msEncrypt.ToArray());
+                    await swEncrypt.WriteAsync(plainText);
                 }
             }
+
+            return Convert.ToBase64String(msEncrypt.ToArray());
         }
 
         public string Decrypt(string cipherText)
         {
-            using (var aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Encoding.UTF8.GetBytes(key);
-                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(_key);
+            aesAlg.IV = Encoding.UTF8.GetBytes(_IV);
 
-                var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                using (var msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
-                {
-                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (var srDecrypt = new StreamReader(csDecrypt))
-                        {
-                            return srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-            }
+            using var msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText));
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            return srDecrypt.ReadToEnd();
         }
     }
 }
