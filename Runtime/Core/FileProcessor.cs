c664bb49a9cee59ecfc8c154ce8f8eda0f97ddf4
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2024-04-02 17:26:41 +0600

    Merge pull request #27 from Studio-23-xyz/dev
    Updated editor wrapped feature

---
+++ Runtime/Core/FileProcessor.cs
@@ -0,0 +1,40 @@
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using Studio23.SS2.SaveSystem.Utilities;
+using System.IO;
+using UnityEngine;
+
+namespace Studio23.SS2.SaveSystem.Core
+{
+    public class FileProcessor : MonoBehaviour
+    {
+        [SerializeField] internal EncryptorBase _encryptor;
+
+        public async UniTask Save<T>(T data, string _saveFilePath)
+        {
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            if (_encryptor != null)
+                json = await _encryptor.Encrypt(json);
+
+            if (File.Exists(_saveFilePath)) File.Delete(_saveFilePath);
+            await File.WriteAllTextAsync(_saveFilePath, json);
+        }
+
+
+        public async UniTask<T> Load<T>(string _saveFilePath)
+        {
+            if (File.Exists(_saveFilePath))
+            {
+                var json = await File.ReadAllTextAsync(_saveFilePath);
+
+                if (_encryptor != null)
+                    json = await _encryptor.Decrypt(json);
+
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+
+            return default;
+        }
+    }
+}
