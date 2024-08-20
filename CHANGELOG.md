32f893ba1e912d04a87aa2f5d4c02b0ee5bfa365
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2024-08-20 15:17:59 +0600

    ## [v2.2.0] - 2024-08-20
    ### Updated
    - Save system now implements optional description to be added

---
+++ CHANGELOG.md
@@ -0,0 +1,208 @@
+# Changelog
+
+## [v2.2.0] - 2024-08-20
+
+### Updated
+- Save system now implements optional description to be added
+
+## [v2.1.9] - 2024-05-13
+
+### Added
+- A new new load method that takes a list of ISavable to grant more control over how the game is loaded
+
+
+## [v2.1.8] - 2024-04-18
+
+### Added
+- An empty constructor is created in save slot data class
+- Added JsonIgore for texture thumbnail in slot data class
+
+
+## [v2.1.6] - 2024-04-08
+
+### Added
+- A new property to know if the slot is empty
+
+## [v2.1.5] - 2024-04-02
+
+### Added
+- Editor Wrapped Feature
+
+## [v2.1.3] - 2024-03-28
+
+### Added
+- OnSaveBegin And OnLoadBegin Events Added
+- When files are saved now they are printed in the debug console + You can directly open file from console.
+
+## [v2.1.1] - 2024-03-20
+
+### Added
+- ISavable Can now call Load self to save itself
+- New Editor feature added ISavable components has a new button that enables loading
+- Saveprocessor is now based on concurrency. Save and Load will now be called from a task queue
+
+
+## [v2.1.0] - 2024-03-18
+
+### Added
+- ISavable Can now call save self to save itself
+- New Editor feature added ISavable components has a new button that enables saving
+
+
+
+## [v2.0.6] - 2024-03-18
+
+### Updated
+- ISavable now Requires Unitask for Load And Save implementation
+
+## [v2.0.4] - 2024-03-07
+
+### Updated
+- Adjusted a dependency update
+
+
+## [v2.0.3] - 2024-01-31
+
+### Updated
+- Code Cleanup and refactor
+- New editor tools for debugging
+- Documentation Update
+- Archive method is now more robust
+
+
+## [v2.0.2] - 2024-01-26
+
+### Updated
+- Readme Updated with proper documentation
+- Bugfix for direct installation via Top Tool Bar
+- Changed use of DateTime.Now to DateTime.UtcNow for performance.
+
+## [v2.0.1] - 2024-01-22
+
+### Added
+
++ Added Cloud Save Core Package as Dependency
++ Added FileProcessor.cs , replacing the deleted SaveProcessor.cs.
++ Added SaveSlotProcessor.cs.
++ Added new samples for demonstration and their configurations.
++ Added Archiver directory and ArchiverBase.cs, ZipUtilityArchiver.cs.
++ Added Encryptor directory and EncryptorBase.cs, AESEncryptor.cs , and deleted Stitcher.cs.
++ Added ItemDataBehaviour.cs and PlayerDataBehaviour.cs scripts for demo.
+
+
+## [v2.0.0] - 2024-01-16
+
+### Updated
+
++ Updated GitHub Actions workflow .github/workflows/UPMBranchUpdate.yml.
++ Modified SaveSystemInstallerWindow.cs script.
++ Updated SaveSystem.cs and SaveSlot.cs.
++ Updated ISaveable.cs interface.
++ Moved FakeData.cs from Tests/PlayMode/Data to Samples/Demo/Scripts and updated metadata.
++ Deleted test scripts BasicSaveLoadTests.cs, EncryptionTest.cs, and SlotMetaDataTest.cs.
+
+### Added
+
++ Added SlotConfiguration.cs.
++ Added FileProcessor.cs , replacing the deleted SaveProcessor.cs.
++ Added SaveSlotProcessor.cs.
++ Added new samples for demonstration and their configurations.
++ Added Archiver directory and ArchiverBase.cs, ZipUtilityArchiver.cs.
++ Added Encryptor directory and EncryptorBase.cs, AESEncryptor.cs , and deleted Stitcher.cs.
++ Added ItemDataBehaviour.cs and PlayerDataBehaviour.cs scripts for demo.
+
+
+
+## [v1.2.0] - 2023-12-14
+
+### Added
+
++  Added Cloud Save Provider
++  Cloud Save Provider Has methods to upload and download files and events to be subscribed
+
+### Updated
++  New API added: Delete Selected Slot
++  New Test added for the new API
+
+## [v1.1.8] - 2023-12-13
+
+### Updated
+
++  Made two internal paths public for extention libraries
+
+
+## [v1.1.7] - 2023-12-7
+
+### Updated
+
++  Now Bundling and Unbundling is based on SharpZipLib from unity
+
+## [v1.1.6] - 2023-10-26
+
+### Fixed
+
++  Fixed a bug regarding enabling encryption
+
+## [v1.1.5] - 2023-10-26
+
+### Updated
+
++  Save Method will now check if directory exists otherwise create it.
+
+
+## [v1.1.3] - 2023-10-26
+
+### Updated
+
++  Updated How Save and Load Methods work.
+
+
+## [v1.1.2] - 2023-10-22
+
+### Added
+
++  Added a new public property called SaveBundle Path. Which can be used by extentions to manage cloud save.
+
+## [v1.1.2] - 2023-10-22
+
+### Added
+
++  New Method added to get save metadata to show UI.
++  New Test Cases Added
+
+
+## [v1.1.1] - 2023-10-22
+
+### Added
+
++  Added SaveEvent delegate and corresponding events for tracking save and load operations.
++  Added methods to handle all components implementing the ISaveable interface, bundle save files into a single file, and extract bundled save files.
+
+### Updated
+
++  Modified SaveSystem class to include new events and methods.
++  Updated SaveData and LoadData methods to invoke events and return loaded data respectively.
++  Updated bundleVersion field in the PlayerSettings structure from 1.1.0 to 1.1.1.
+
+
+
+## [v1.1.0] - 2023-10-21
+
+### Added
+
+- New SaveSystem Class added which will be the new entrypoint of using this  library
+- New Test Cases to ensure the library works as intented.
+
+### Updated
+
+- Updated how Bundling and unbundeling works.
+
+### Fixed
+
+- Now Bundling and unbundeling works properly
+
+### Removed
+
+- Bugs
+
+
---
+++ Runtime/Core/SaveSlotProcessor.cs
@@ -0,0 +1,346 @@
+using Cysharp.Threading.Tasks;
+using Studio23.SS2.CloudSave.Core;
+using Studio23.SS2.SaveSystem.Data;
+using Studio23.SS2.SaveSystem.Interfaces;
+using Studio23.SS2.SaveSystem.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+#if UNITY_EDITOR
+using Needle;
+#endif
+using UnityEngine;
+
+[assembly: InternalsVisibleTo("com.studio23.ss2.savesystem.editor")]
+
+namespace Studio23.SS2.SaveSystem.Core
+{
+    [RequireComponent(typeof(FileProcessor))]
+    internal class SaveSlotProcessor : MonoBehaviour
+    {
+        private static readonly string LastSelectedSaveSlotPrefsKey = "lasSelectedSlotIndex";
+        
+        [SerializeField] 
+        internal ArchiverBase _archiverBase;
+
+        [SerializeField] 
+        internal CloudSaveManager _cloudSaveManager;
+        [SerializeField] 
+        internal FileProcessor _fileProcessor;
+
+        [SerializeField] 
+        internal SaveSlot _selectedSlot;
+        [SerializeField] 
+        internal SlotConfiguration _slotConfiguration;
+        internal string SavePathRoot;
+
+
+
+        private readonly Queue<UniTask> _taskQueue = new Queue<UniTask>();
+        private bool _isworking;
+
+
+        internal async UniTask SelectSlot(int index)
+        {
+            _selectedSlot = await GetSaveSlotMetaDataLocal(index);
+            PlayerPrefs.SetInt(LastSelectedSaveSlotPrefsKey, index);
+        }
+
+        internal async UniTask SelectLastSelectedSlot()
+        {
+            var index = PlayerPrefs.GetInt(LastSelectedSaveSlotPrefsKey, 0);
+            await SelectSlot(index);
+        }
+
+        #region Load
+
+        internal async UniTask LoadAllSavable()
+        {
+            var savableComponents = FindObjectsOfType<MonoBehaviour>(true).OfType<ISaveable>();
+
+            await LoadISavables(savableComponents);
+        }
+
+        internal async UniTask LoadISavables(IEnumerable<ISaveable> savableComponents)
+        {
+            await QueueTask(LoadTask(savableComponents));
+        }
+
+        private async UniTask LoadTask(IEnumerable<ISaveable> savableComponents)
+        {
+            foreach (var savableComponent in savableComponents)
+            {
+                var key = savableComponent.GetUniqueID();
+                var filepath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDatafolderName,
+                    $"{key}{_slotConfiguration.SaveFileExtention}");
+                if (!File.Exists(filepath))
+                {
+                    await savableComponent.ManageSaveLoadException(new FileNotFoundException(
+                        $"ISavable <color=white>Key: {key}</color> Method: <color=yellow>LoadAllSavable()</color>"));
+                }
+
+                var data = await _fileProcessor.Load<string>(filepath);
+                await savableComponent.AssignSerializedData(data);
+            }
+
+            if (_slotConfiguration.EnableBackups)
+            {
+                Debug.Log("Save Integrity Validated,<color=yellow> Creating Backup </color>");
+                CreateBackup().Forget();
+            }
+        }
+
+        #endregion
+
+        #region Setup
+
+        internal string GetSelectedSlotPath()
+        {
+            return Path.Combine(SavePathRoot, _selectedSlot.Name);
+        }
+
+        internal async UniTask<SaveSlot> GetSaveSlotMetaDataLocal(int index)
+        {
+            var slotMeta = new SaveSlot(index);
+
+            var slotMetaPath = Path.Combine(SavePathRoot, slotMeta.Name, _slotConfiguration.SlotMetafileName);
+            if (File.Exists(slotMetaPath)) slotMeta = await _fileProcessor.Load<SaveSlot>(slotMetaPath);
+
+            return slotMeta;
+        }
+
+        public async UniTask Initialize()
+        {
+            SavePathRoot = Path.Combine(Application.persistentDataPath, _slotConfiguration.SaveRootFolderName);
+            _fileProcessor = GetComponent<FileProcessor>();
+            CreateSlotFolders();
+            await _cloudSaveManager.Initialize();
+            await GetAllCloudMeta();
+        }
+
+        internal void CreateSlotFolders()
+        {
+            for (var i = 0; i < _slotConfiguration.SlotCount; i++)
+            {
+                var slot = new SaveSlot(i);
+
+                var slotPath = Path.Combine(SavePathRoot, slot.Name, _slotConfiguration.SlotDatafolderName);
+                if (Directory.Exists(slotPath)) continue;
+                Directory.CreateDirectory(slotPath);
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        internal async UniTask ClearAllSlotsAsync()
+        {
+            for (var i = 0; i < _slotConfiguration.SlotCount; i++)
+            {
+                var filepath = Path.Combine(SavePathRoot, $"Save_Slot_{i}");
+                if (Directory.Exists(filepath)) await UniTask.RunOnThreadPool(() => Directory.Delete(filepath, true));
+            }
+
+            CreateSlotFolders();
+        }
+
+        internal async UniTask ClearSelectedSlotAsync()
+        {
+            var filepath = GetSelectedSlotPath();
+            await UniTask.RunOnThreadPool(() => Directory.Delete(filepath, true));
+            CreateSlotFolders();
+        }
+
+        internal async UniTask ClearSelectedSlotCloudAsync()
+        {
+            await _cloudSaveManager.DeleteContainerFromCloud(_selectedSlot.Name);
+        }
+
+
+#if UNITY_EDITOR
+        /// <summary>
+        ///     Should only be used for debug purposes. This does not update meta file keys
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        internal async UniTask DeleteKeyFromSelectedSlot(string key)
+        {
+            var filepath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDatafolderName,
+                $"{key}{_slotConfiguration.SaveFileExtention}");
+            await UniTask.RunOnThreadPool(() => File.Delete(filepath));
+        }
+#endif
+
+        #endregion
+
+        #region Save
+
+        internal async UniTask SaveAllSavable(bool dirtyOnly,string description)
+        {
+            var savableComponents = FindObjectsOfType<MonoBehaviour>(true).OfType<ISaveable>();
+            if (dirtyOnly) savableComponents = savableComponents.Where(r => r.IsDirty);
+            await SaveISavables(savableComponents, description);
+        }
+
+        internal async UniTask SaveISavables(IEnumerable<ISaveable> savableComponents,string description)
+        {
+            await QueueTask(SaveTask(savableComponents,description));
+        }
+
+        private async UniTask SaveTask(IEnumerable<ISaveable> savableComponents,string description)
+        {
+            foreach (var savableComponent in savableComponents)
+            {
+                var key = savableComponent.GetUniqueID();
+                string data = await savableComponent.GetSerializedData();
+
+                var filepath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDatafolderName,
+                    $"{key}{_slotConfiguration.SaveFileExtention}");
+
+                await _fileProcessor.Save(data, filepath);
+                Debug.Log($"<color=green>Saved</color> With Key: {key} ");
+#if UNITY_EDITOR
+                Debug.Log($"{"<color=white>Open File</color>".LinkTo(filepath)}");
+#endif
+                _cloudSaveManager.UploadToCloud(_selectedSlot.Name, $"{key}", filepath).Forget();
+
+                _selectedSlot.FileKeys[$"{key}"] = Encoding.Unicode.GetByteCount(data);
+
+                savableComponent.IsDirty = false;
+            }
+
+            await SaveSelectedSlotMetadata(description);
+        }
+
+        private async UniTask SaveSelectedSlotMetadata(string description)
+        {
+            _selectedSlot.Description= description;
+            _selectedSlot.TimeStamp = DateTime.UtcNow;
+            var slotPath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotMetafileName);
+            await _fileProcessor.Save(_selectedSlot, slotPath);
+            _cloudSaveManager.UploadToCloud(_selectedSlot.Name, _slotConfiguration.SlotMetafileName, slotPath).Forget();
+        }
+
+        #endregion
+
+        #region Cloud Methods
+
+        internal async UniTask GetAllCloudMeta()
+        {
+            var cloudMetaDownloadTaskList = new List<UniTask>();
+
+            for (var i = 0; i < _slotConfiguration.SlotCount; i++)
+            {
+                var slotMeta = new SaveSlot(i);
+                var slotMetaPath = Path.Combine(SavePathRoot, slotMeta.Name, _slotConfiguration.SlotMetafileName);
+                var metafileTask = _cloudSaveManager.DownloadFromCloud(slotMeta.Name,
+                    _slotConfiguration.SlotMetafileName, slotMetaPath);
+
+                cloudMetaDownloadTaskList.Add(metafileTask);
+            }
+
+            await UniTask.WhenAll(cloudMetaDownloadTaskList);
+        }
+
+        internal async UniTask SyncSelectedSlotData()
+        {
+            var datafileDownloadTaskList = new List<UniTask>();
+
+            foreach (var key in _selectedSlot.FileKeys.Keys)
+            {
+                var filePath = Path.Combine(SavePathRoot, _selectedSlot.Name, _slotConfiguration.SlotDatafolderName,
+                    $"{key}{_slotConfiguration.SaveFileExtention}");
+                var fileTask = _cloudSaveManager.DownloadFromCloud(_selectedSlot.Name, key, filePath);
+                datafileDownloadTaskList.Add(fileTask);
+            }
+
+            await UniTask.WhenAll(datafileDownloadTaskList);
+        }
+
+        internal async UniTask CreateBackup()
+        {
+            var dataFolderPath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDatafolderName);
+            var backupFilePath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDataBackupFileName);
+
+
+            await _archiverBase.ArchiveFiles(backupFilePath, dataFolderPath);
+            Debug.Log($"<color=green>Backup</color> Created at {backupFilePath}");
+#if UNITY_EDITOR
+            Debug.Log($"Open File {backupFilePath.LinkTo(backupFilePath)}");
+#endif
+
+            _cloudSaveManager
+                .UploadToCloud(_selectedSlot.Name, $"{_slotConfiguration.SlotDataBackupFileName}", backupFilePath)
+                .Forget();
+
+            _selectedSlot.HasBackup = true;
+            _selectedSlot.BackupStamp = DateTime.UtcNow;
+
+            await SaveSelectedSlotMetadata(_selectedSlot.Description);
+        }
+
+        internal async UniTask RestoreBackup()
+        {
+            var dataFolderPath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDatafolderName);
+            var backupFilePath = Path.Combine(GetSelectedSlotPath(), _slotConfiguration.SlotDataBackupFileName);
+
+            if (!File.Exists(backupFilePath))
+            {
+                Debug.Log("Backup File not found, Attempting to restore from cloud");
+                await _cloudSaveManager.DownloadFromCloud(_selectedSlot.Name,
+                    $"{_slotConfiguration.SlotDataBackupFileName}", backupFilePath);
+            }
+
+            if (!File.Exists(backupFilePath))
+            {
+                Debug.Log("Backup file doesn't exist on cloud either! Cry.");
+                throw new Exception("Cloud Save doesn't exist, Provide proper UI");
+            }
+
+
+            await _archiverBase.ExtractFiles(backupFilePath, dataFolderPath);
+        }
+
+        #endregion
+
+
+        //TODO : Create Unit Tests
+
+        #region Concurrency
+
+        private async UniTask QueueTask(UniTask task)
+        {
+            _taskQueue.Enqueue(task);
+            if (!_isworking)
+            {
+                _isworking = true;
+                await ProcessSaveQueue();
+            }
+        }
+
+
+        private async UniTask ProcessSaveQueue()
+        {
+            while (_taskQueue.Count > 0)
+            {
+                await _taskQueue.Dequeue();
+            }
+
+            _isworking = false;
+
+        }
+
+
+
+        #endregion
+
+
+
+
+
+    }
+}
